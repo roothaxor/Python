cfb466de3533bb502c87d0d5efeb7e9a415810e3
SanJay <indianblogger786@gmail.com>
2017-04-08 12:50:32 -1000

    Update exif_remove.py
    Copy this script and paste to directory of your files and run, simple auto script

--- exif_remove.py
+++ exif_remove.py
@@ -5,10 +5,13 @@
 from PIL import Image
 import os, sys
 
+'''Old works single file work, sucks
+
 image_file = raw_input(" Enter the file: ")
 
 if os.path.isfile(image_file):
 
+	
 	directory, filename = os.path.split(image_file)
 
 	image = Image.open(image_file)
@@ -19,12 +22,27 @@ if os.path.isfile(image_file):
 
 	image_without_exif.putdata(data)
 
-	image_without_exif.save(directory + "/x_" + filename)
+	image_without_exif.save(directory + "/notrace_" + filename)
 
 	print(" file saved: %s/x_%s" % (directory, filename))
 
 	sys.exit(0)
 else:
-	print(" File Path not correct or found try again bitch !!!")
+	print(" File Path not correct or found try again Sucka !")
 	
-	sys.exit(1)
+	sys.exit(1)'''
+#exif remove data from files directorys
+relevant_path = os.getcwd()
+included_extenstions = ['jpg', 'bmp', 'png', 'gif']
+l = [files for files in os.listdir(relevant_path)
+          	if any(files.endswith(ext) for ext in included_extenstions)]
+for image_file in l:
+	image  = Image.open(image_file)
+	data = list(image.getdata())
+	image_without_exif = Image.new(image.mode, image.size)
+	image_without_exif.putdata(data)
+	directory = os.getcwd()
+	image_without_exif.save(directory + "/ex_" + image_file)
+	print "Exif data removed, saved as %s\ex_%s" % (directory, image_file)
+	delete = os.remove(image_file)
+print "\n\nFiles with exif data deleted automatically, thankyou"
